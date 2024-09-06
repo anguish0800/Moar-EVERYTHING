85d5ea4ec610b302a5a7f0b68ad6fa3dc7f3bd9d
Glitch (immense-shaded-flare) <none>
2024-09-06 14:49:53 +0000

    🌔🏩 Checkpoint ./server/modules/definitions/addons/moar.js:67347270/1818 ./server/modules/definitions/facilitators.js:67347270/977

---
+++ server/modules/definitions/addons/moar.js
@@ -0,0 +1,32 @@
+const { combineStats, makeAuto, makeOver, makeDeco, makeGuard, makeBird, makeMulti, makeRadialAuto } = require('../facilitators.js');
+const { base, statnames, gunCalcNames, dfltskl, smshskl } = require('../constants.js');
+require('./generics.js');
+const g = require('../gunvals.js');
+
+// flanks
+// snipers
+Class.degrader = makeMulti("sniper", 3, "Degrader")
+Class.rustage = makeMulti("assassin", 3, "Rustage")
+Class.prey = makeMulti("hunter", 3, "Prey")
+Class.machShot = makeMulti("minigun", 3, "Mach-Shot")
+Class.assault = makeMulti("rifle", 3, "Assault")
+Class.breaker = makeMulti("sniper", 6, "Breaker")
+Clsss.
+// machine guns
+Class.sabotager = makeMulti("machineGun", 3, "Sabotager")
+Class.foctar = makeMulti("artillery", 3, "Foctar")
+Class.vase = makeMulti("gunner", 3, "Vase")
+Class.ultimatum = makeMulti("sprayer", 3, "Ultimatum")
+Class.shower = makeMulti("machineGun", 6, "Shower")
+// pounders
+Class.obliterator = makeMulti("pounder", 3, "Obliterator")
+Class.demolitionist = makeMulti("destroyer", 3, "Demolitionist")
+Class.exterminator = makeMulti("launcher", 3, "Exterminator")
+Class.eradicator = makeMulti("pounder", 6, "Eradicator")
+
+// upgrade paths
+Class.sniper.UPGRADES_TIER_2.push("degrader")
+		Class.degrader.UPGRADER_TIER_3 = ["rustage", "prey", "machShot", "assault", "breaker"]
+		Class.assassin.UPGRADES_TIER_3.push("rustage")
+		Class.hunter.UPGRADES_TIER_3.push("prey")
+		Class.rifle.UPGRADES_TIER_3.push("assault")
